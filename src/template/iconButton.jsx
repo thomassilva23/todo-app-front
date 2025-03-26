6227807309a82d5325e8c147e6afb1a0f57c1f27
Thomas Silva <thomas@allto.digital>
2025-03-26 14:08:39 -0300

    Refatoração do componente IconButton para usar o componente If para renderização condicional; handleAdd para TodoForm

--- src/template/iconButton.jsx
+++ src/template/iconButton.jsx
@@ -1,13 +1,10 @@
 import React from "react";
+import If from "./if";
 
 export default (props) => {
-  if (props.hide) {
-    return null;
-  } else {
-    return (
-      <button className={"btn btn-" + props.style} onClick={props.onClick}>
-        <i className={"fa fa-" + props.icon}></i>
-      </button>
-    );
-  }
+  <If test={!props.hide}>
+    <button className={"btn btn-" + props.style} onClick={props.onClick}>
+      <i className={"fa fa-" + props.icon}></i>
+    </button>
+  </If>;
 };
